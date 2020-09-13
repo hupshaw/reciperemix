262857aa8299fa8b5c857fd3d41202a5b34da406
Helena Upshaw <helenaupshaw2@gmail.com>
2020-09-12 21:19:32 -0400

    Updates

---
+++ precache-manifest.2840a5d28c15f1edbc610d783479b507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efca76d74c6730bda0bf70e721c8454",
+    "url": "/RecipeRemix/index.html"
+  },
+  {
+    "revision": "4fb28a482126579f9f66",
+    "url": "/RecipeRemix/static/css/main.e6c68330.chunk.css"
+  },
+  {
+    "revision": "57f987e00ba147f36149",
+    "url": "/RecipeRemix/static/js/2.d37b1e0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeRemix/static/js/2.d37b1e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb28a482126579f9f66",
+    "url": "/RecipeRemix/static/js/main.d9e1c3dd.chunk.js"
+  },
+  {
+    "revision": "37664cc30940be829a78",
+    "url": "/RecipeRemix/static/js/runtime-main.e67499b2.js"
+  }
+]);
