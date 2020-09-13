1c8c00f9fde3b6f7579b43a2a2ec3a4bb2519fb4
Helena Upshaw <helenaupshaw2@gmail.com>
2020-09-12 21:35:06 -0400

    Updates

---
+++ precache-manifest.49b0ba0d0be9870cf416f35a98f5968e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2fb39a375b977fce13055059891fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fb28a482126579f9f66",
+    "url": "/static/css/main.e6c68330.chunk.css"
+  },
+  {
+    "revision": "57f987e00ba147f36149",
+    "url": "/static/js/2.d37b1e0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d37b1e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb28a482126579f9f66",
+    "url": "/static/js/main.d9e1c3dd.chunk.js"
+  },
+  {
+    "revision": "cafa969bf7eef8e2e35e",
+    "url": "/static/js/runtime-main.18f78759.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecipeRemix/precache-manifest.2840a5d28c15f1edbc610d783479b507.js"
+  "/precache-manifest.49b0ba0d0be9870cf416f35a98f5968e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RecipeRemix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
